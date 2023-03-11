32ce90ecbc0fae717f3590a424a9e5abc0e0e2b0
Radu Capatina <raduandreicapatina@yahoo.com>
2023-03-11 09:31:43 +0200

    patch

--- src/main/java/me/raducapatina/server/MainServer.java
+++ src/main/java/me/raducapatina/server/MainServer.java
@@ -4,7 +4,6 @@ import me.raducapatina.server.core.ServerInstance;
 
 public class MainServer {
 
-
     public static void main(String[] args) {
         ServerInstance serverInstance = new ServerInstance();
         serverInstance.start();
